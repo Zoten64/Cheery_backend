671a16e8302f558b1cffe7192c0e6dbafa1f731d
Zoten64 <forjavligratta@gmail.com>
2024-07-09 09:28:32 +0200

    Feat: ProfileList and ProfileDetails views - Docs: Add cloudinary and Pillow to libraries section

--- profiles/views.py
+++ profiles/views.py
@@ -4,9 +4,11 @@ from .serializers import ProfileSerializer
 
 # Create your views here.
 class ProfileList(generics.ListAPIView):
+    '''List all profiles'''
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 class ProfileDetail(generics.RetrieveAPIView):
+    '''Retrieve a specific profile'''
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
